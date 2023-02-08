5d7ddf63be687e4a3ffea82eb945e623f37e61b3
Andres Garcia Paredes <50889119+andrepared@users.noreply.github.com>
2023-02-08 16:19:41 -0600

    Operations folder

---
+++ Operations/Artithmetic.java
@@ -0,0 +1,14 @@
+
+public class Artithmetic {
+    public static void main(String args[])
+    {
+
+        int a = 14, b = 5;
+
+        int c = a / b;
+        int r = a % b;
+
+        System.out.println(c);
+
+    }
+}
