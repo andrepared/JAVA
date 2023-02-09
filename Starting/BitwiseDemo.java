ba439138785e6fce33b315ef5f828428166c1cd5
Andres Garcia Paredes <50889119+andrepared@users.noreply.github.com>
2023-02-09 16:15:54 -0600

    You could write any number in binary form

--- Starting/BitwiseDemo.java
+++ Starting/BitwiseDemo.java
@@ -4,11 +4,13 @@
 
 class BitwiseDemo
 
-{
+{git add
 
     public static void main(String args[])
     {
-        int x = 10, y = 6, z;
+        int x =0b1010,
+            y =0b0110,
+            z;
         z = x & y;
         System.out.println(z);  
 
