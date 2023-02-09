59e9d445041e7ca9a802fb93ff3d9dcacec38497
Andres Garcia Paredes <50889119+andrepared@users.noreply.github.com>
2023-02-09 16:28:02 -0600

    String to Binary

--- Starting/BitwiseDemo.java
+++ Starting/BitwiseDemo.java
@@ -7,11 +7,12 @@ class BitwiseDemo
 
     public static void main(String args[])
     {
-        int x =0b1000,
+        int x =-0b1010,
          y; 
 
-        y=x<<2;
-        System.out.println(y);  
+        y=x>>>1;
+        System.out.println(String.format("%s", Integer.toBinaryString(x)));  
+        System.out.println(String.format("%s", Integer.toBinaryString(y)));  
 
     }
     
