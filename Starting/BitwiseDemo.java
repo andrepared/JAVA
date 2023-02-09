8e0a20f90cfbcfc08e081de6dd8f1bd9a37c520d
Andres Garcia Paredes <50889119+andrepared@users.noreply.github.com>
2023-02-09 16:13:22 -0600

    Bitwise operations

---
+++ Starting/BitwiseDemo.java
@@ -0,0 +1,17 @@
+
+
+
+
+class BitwiseDemo
+
+{
+
+    public static void main(String args[])
+    {
+        int x = 10, y = 6, z;
+        z = x & y;
+        System.out.println(z);  
+
+    }
+    
+}
