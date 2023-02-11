d930604d075752136b7ca0fe16af05d15ee21558
Andres Garcia Paredes <50889119+andrepared@users.noreply.github.com>
2023-02-11 15:36:35 -0600

    Practicing Switch statement

--- relational/PracticingSwitch.java
+++ relational/PracticingSwitch.java
@@ -4,25 +4,25 @@ public class PracticingSwitch {
     public static void main(String args[])
     {
 
-       String option = "Save";
+       String option = "Hello";
 
-switch (option)
-{
-    case "Save":
-        System.out.println("This is the save option");
-            break;
-    case "New":
-        System.out.println("This is the new option");
-            break;
-    case "Save As":
-        System.out.println("This is the save as option");
-            break;
-    case "Close":
-        System.out.println("This is the close option");
-            break;
-        default:
-            System.out.println("This is not an option")
-}
+            switch (option)
+            {
+                case "Save":
+                    System.out.println("This is the save option");
+                        break;
+                case "New":
+                    System.out.println("This is the new option");
+                        break;
+                case "Save As":
+                    System.out.println("This is the save as option");
+                        break;
+                case "Close":
+                    System.out.println("This is the close option");
+                        break;
+                    default:
+                        System.out.println("This is not an option");
+            }
 
     }
 
