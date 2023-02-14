fbd24a48176c8da1a28864b5f03a820bebc48015
Andres Garcia Paredes <50889119+andrepared@users.noreply.github.com>
2023-02-14 10:27:06 -0600

    Practicing algorithm that shifts elements after deletion.

--- Arrays/DeleteAndShift.java
+++ Arrays/DeleteAndShift.java
@@ -4,35 +4,52 @@ import java.util.Arrays;
 
 public class DeleteAndShift {
 
-    public static void main (String args[])
-    {
-        int[] arr = { 3, 1, 6, 5, 2, 8, 4 };
+    public static void main(String args[]) {
+        // int[] arr = { 3, 1, 6, 5, 2, 8, 4 };
+        // int[] newArr = null;
+        // int elementToBeRemoved = 5;
+        // System.out.println("Original Array is: " + Arrays.toString(arr));
+
+        // for(int i = 0; i < arr.length-1; i++)
+        // {
+        // if(arr[i]== elementToBeRemoved)
+        // {
+        // newArr = new int[arr.length - 1];
+        // for(int index=0; index<i; index++)
+        // {
+        // newArr[index] = arr[index];
+        // }
+        // for (int j = i; j < arr.length -1; j++) {
+        // newArr[j] = arr[j + 1];
+
+        // }
+        // break;
+        // }
+        // }
+        // System.out.println("New Array after deleting element = " +elementToBeRemoved
+        // +
+        // " and shifting: " + Arrays.toString(newArr));
+        int[] original = { 1, 3, 6, 5, 8, 8, 2, 4 };
         int[] newArr = null;
-        int elementToBeRemoved = 5;
-        System.out.println("Original Array is: " + Arrays.toString(arr));
+        int key = 8;
+        System.out.println("This is the original array: " + Arrays.toString(original));
 
-
-        for(int i = 0; i < arr.length-1; i++)
-        {
-            if(arr[i]== elementToBeRemoved)
-            {
-                newArr = new int[arr.length - 1];
-                for(int index=0; index<i; index++)
-                {
-                    newArr[index] = arr[index];
+        // Constructing new array
+        for (int i = 0; i < original.length - 1; i++) {
+            if (original[i] == key) {
+                newArr = new int[original.length - 1];
+                for (int index = 0; index < i; index++) {
+                    newArr[index] = original[index];
                 }
-                for (int j = i; j < arr.length -1; j++) {
-                    newArr[j] = arr[j + 1];
-
+                // shifting elements
+                for (int j = i; j < original.length - 1; j++) {
+                    newArr[j] = original[j + 1];
                 }
                 break;
             }
         }
-    System.out.println("New Array after deleting element = " +elementToBeRemoved +
-                        " and shifting: " + Arrays.toString(newArr));
-
-
-
+        System.out.println("New Array after deleting element = " + key +
+                " and shifting: " + Arrays.toString(newArr));
     }
 
 }
