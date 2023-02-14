ab65de7a84886dc7f3e3232f3a410dd24756a96a
Andres Garcia Paredes <50889119+andrepared@users.noreply.github.com>
2023-02-14 11:29:15 -0600

    Removing element with Array List

--- Arrays/DeleteAndShift.java
+++ Arrays/DeleteAndShift.java
@@ -1,6 +1,7 @@
 package Arrays;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class DeleteAndShift {
 
@@ -29,27 +30,26 @@ public class DeleteAndShift {
         // System.out.println("New Array after deleting element = " +elementToBeRemoved
         // +
         // " and shifting: " + Arrays.toString(newArr));
-        int[] original = { 1, 3, 6, 5, 8, 8, 2, 4 };
-        int[] newArr = null;
-        int key = 8;
-        System.out.println("This is the original array: " + Arrays.toString(original));
 
-        // Constructing new array
-        for (int i = 0; i < original.length - 1; i++) {
-            if (original[i] == key) {
-                newArr = new int[original.length - 1];
-                for (int index = 0; index < i; index++) {
-                    newArr[index] = original[index];
-                }
-                // shifting elements
-                for (int j = i; j < original.length - 1; j++) {
-                    newArr[j] = original[j + 1];
-                }
+        // Use ArrayList to Remove Elemnt from Arrau and Shift in Java
+        Integer[] arr = { 3, 1, 6, 4, 2, 8 };
+        int elementToBeRemoved = 2;
+
+        System.out.println("Original Array: " + Arrays.toString(arr));
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == elementToBeRemoved) {
+                arr = removeElementUsingCollection(arr, i);
                 break;
             }
+            System.out.println("Array after removing element: " + elementToBeRemoved);
+            for (int j = 0; j < arr.length; j++) {
+                System.out.println(" " + arr[j]);
+            }
         }
-        System.out.println("New Array after deleting element = " + key +
-                " and shifting: " + Arrays.toString(newArr));
     }
-
+    public static Integer[] removeElementUsingCollection(Integer[] arr, int index) {
+        List<Integer> tempList = new ArrayList<Integer>(Arrays.asList(arr));
+        tempList.remove(index);
+        return tempList.toArray(new Integer[0]);
+    }
 }
