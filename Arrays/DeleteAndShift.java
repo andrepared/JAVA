f41152381580df268d0c1608c44a3efe41bb3ee1
Andres Garcia Paredes <50889119+andrepared@users.noreply.github.com>
2023-02-14 10:10:12 -0600

    Modified code for shifting elements in Array

--- Arrays/DeleteAndShift.java
+++ Arrays/DeleteAndShift.java
@@ -4,26 +4,35 @@ import java.util.Arrays;
 
 public class DeleteAndShift {
 
-    public static void main(String args[]) {
-        int arr[] = { 1, 2, 3, 4, 5, 7, 8 };
-        int j = 3;
-        System.out.println("Before deletion" + Arrays.toString(arr));
-        int count = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == j) {
-                count++;
-            }
-            for (int k = i; k < arr.length; k++) {
-                arr[k] = arr[k + 1];
-            }
-            count--;
+    public static void main (String args[])
+    {
+        int[] arr = { 3, 1, 6, 5, 2, 8, 4 };
+        int[] newArr = null;
+        int elementToBeRemoved = 5;
+        System.out.println("Original Array is: " + Arrays.toString(arr));
 
-        }
-        System.out.println("After deletion: ");
-        for (int i = 0; i < arr.length - count; i++) {
-            System.out.println(" " + arr[i]);
-        }
 
-        System.out.println("Entire Array: " + Arrays.toString(arr));
+        for(int i = 0; i < arr.length-1; i++)
+        {
+            if(arr[i]== elementToBeRemoved)
+            {
+                newArr = new int[arr.length - 1];
+                for(int index=0; index<i; index++)
+                {
+                    newArr[index] = arr[index];
+                }
+                for (int j = i; j < arr.length -1; j++) {
+                    newArr[j] = arr[j + 1];
+
+                }
+                break;
+            }
+        }
+    System.out.println("New Array after deleting element = " +elementToBeRemoved +
+                        " and shifting: " + Arrays.toString(newArr));
+
+
+
     }
+
 }
