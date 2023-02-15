5f8ebc840a1de314da1dd560552b0d58fa381596
Andres Garcia Paredes <50889119+andrepared@users.noreply.github.com>
2023-02-15 15:11:28 -0600

    For each loop to display 2D array

--- Arrays/TwoDArrays.java
+++ Arrays/TwoDArrays.java
@@ -6,13 +6,19 @@ public class TwoDArrays {
         int[][] A = new int[2][5];
         int[][] B = { { 1, 2, 3, 4, 5 }, { 3, 6, 1, 4, 5 } };
         
-       for(int i=0;i<B.length;i++){
+    //    for(int i=0;i<B.length;i++){
 
-           for (int j = 0; j < B[0].length; j++) {
-               System.out.println(B[i][j]);
+    //        for (int j = 0; j < B[0].length; j++) {
+    //            System.out.println(B[i][j]);
+    //         }
+    //     }
+            for(int x[]:B)
+            {
+            for(int y:x)
+            {
+                System.out.println(y);
             }
         }
-
 
     }
 }
