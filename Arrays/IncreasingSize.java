ed70711e6343f1dc367db8f8c9f46eec0693ef37
Andres Garcia Paredes <50889119+andrepared@users.noreply.github.com>
2023-02-15 15:06:38 -0600

    Array practice

---
+++ Arrays/IncreasingSize.java
@@ -0,0 +1,11 @@
+package Arrays;
+
+public class IncreasingSize {
+    public static void main(String args[])
+    {
+        int A[] = { 8, 6, 10, 9, 2 };
+        int B[] = new int[2*A.length];
+
+         
+    }
+}
---
+++ Arrays/TwoDArrays.java
@@ -0,0 +1,18 @@
+package Arrays;
+
+public class TwoDArrays {
+    public static void main(String args[]) {
+
+        int[][] A = new int[2][5];
+        int[][] B = { { 1, 2, 3, 4, 5 }, { 3, 6, 1, 4, 5 } };
+        
+       for(int i=0;i<B.length;i++){
+
+           for (int j = 0; j < B[0].length; j++) {
+               System.out.println(B[i][j]);
+            }
+        }
+
+
+    }
+}
