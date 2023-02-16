559eabf3d7727c9a494209196e8ba1c8839e2cb3
Andres Garcia Paredes <50889119+andrepared@users.noreply.github.com>
2023-02-16 11:33:04 -0600

    Sorted Merged Array

--- Arrays/MergingTwoArrays.java
+++ Arrays/MergingTwoArrays.java
@@ -1,26 +1,38 @@
 package Arrays;
 
+import java.util.Arrays;
+
 public class MergingTwoArrays {
     public static void main(String args[]) {
-
+        int i = 0, j = 0, k = 0;
         int a[] = { 30, 40, 50, 60 };
         int b[] = { 45, 50, 55, 60, 65 };
 
-        int a1 = a.length;
-        int b1 = b.length;
+        int m = a.length;
+        int n = b.length;
 
-        int c1 = a1 + b1;
-
+        int c1 = m + n;
         int c[] = new int[c1];
 
-        for (int i = 0; i < a1; i++) {
-            c[i] = a[i];
+        while (i < m && j < n) {
+            if (a[i] < b[j]) {
+                c[k++] = a[i++];
+            } else
+                c[k++] = b[j++];
         }
-        for (int i = 0; i < b1; i++) {
-            c[a1 + i] = b[i];
+
+        for (; i < m; i++) {
+            c[k++] = a[i];
         }
-        for (int i = 0; i < c1; i++) {
-            System.out.println(c[i]);
+        for (; j < n; j++) {
+            c[k++] = b[j];
+        }
+        System.out.println("Array A is: " + Arrays.toString(a));
+        System.out.println("Array B is: " + Arrays.toString(b));
+        System.out.println();
+        System.out.println("Sorted Merged Array C is:\n");
+        for (k = 0; k < c1; k++) {
+            System.out.println(c[k]);
         }
     }
 }
