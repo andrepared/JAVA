f3577a9091d5c99ae0b2f32938db3c97cb3d2ce9
Andres Garcia Paredes <50889119+andrepared@users.noreply.github.com>
2023-02-24 15:51:57 -0600

    Methods

--- Arrays/ArrangeStrings.java
+++ Arrays/ArrangeStrings.java
@@ -8,5 +8,13 @@ public class ArrangeStrings {
         for (String x : arr)
             System.out.println(x);
 
+        // // }
+        // public static void main(String args[]) {
+        //     String[] arr = { "java", "pascal", "python", "smallTalk", "ada", "basic" };
+
+        //     java.util.Arrays.sort(arr);
+        //     for(String x: arr)
+        //         System.out.println(x);
+        // }
     }
 }
---
+++ Methods/MethodPractice.java
@@ -0,0 +1,25 @@
+package Methods; 
+
+public class MethodPractice {
+    // static void update(int A[]) {
+    //     A[0] = 25;
+    // }
+
+    // public static void main(String args[]) {
+    //     int A[] = { 1, 2, 3, 4, 5, 6 };
+    //     update(A);
+    //     System.out.println(A[0]);
+    //     }
+    int max(int x, int y)
+    {
+        return x > y ? x : y;
+    }
+
+    String userName(String email)
+    {
+        int a = email.indexOf("@");
+        String name = email.substring(0, a);
+        return name;
+    }
+
+}
