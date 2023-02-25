2fc8fd160072b89eccb02b8afc57b5080f224ba2
Andres Garcia Paredes <50889119+andrepared@users.noreply.github.com>
2023-02-25 10:21:47 -0600

    Method Practice

--- Methods/MethodPractice.java
+++ Methods/MethodPractice.java
@@ -10,17 +10,29 @@ public class MethodPractice {
     //     update(A);
     //     System.out.println(A[0]);
     //     }
-    int max(int x, int y)
-    {
-        return x > y ? x : y;
-    }
+    // int max(int x, int y)
+    // {
+    //     return x > y ? x : y;
+    // }
 
-    String userName(String email)
-    {
-        int a = email.indexOf("@");
-        String name = email.substring(0, a);
-        return name;
+    // String userName(String email)
+    // {
+    //     int a = email.indexOf("@");
+    //     String name = email.substring(0, a);
+    //     return name;
 
+    // }
+    static void change(int A[], int index, int value){
+        A[index]=value;
+        }
+
+     static void main(String[] args) {
+        int A[] = { 1, 2, 3, 4, 5 };
+
+        change(A,2,20);
+
+    for(int x:A)
+        System.out.println(x);
     }
 
 }
