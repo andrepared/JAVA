0d22effe45e13b834ab2708199f731a8b03364fa
Andres Garcia Paredes <50889119+andrepared@users.noreply.github.com>
2023-02-25 10:41:32 -0600

    Practicing Methods

--- Methods/MethodPractice.java
+++ Methods/MethodPractice.java
@@ -1,38 +1,42 @@
-package Methods; 
+package Methods;
 
 public class MethodPractice {
     // static void update(int A[]) {
-    //     A[0] = 25;
+    // A[0] = 25;
     // }
 
     // public static void main(String args[]) {
-    //     int A[] = { 1, 2, 3, 4, 5, 6 };
-    //     update(A);
-    //     System.out.println(A[0]);
-    //     }
+    // int A[] = { 1, 2, 3, 4, 5, 6 };
+    // update(A);
+    // System.out.println(A[0]);
+    // }
     // int max(int x, int y)
     // {
-    //     return x > y ? x : y;
+    // return x > y ? x : y;
     // }
 
     // String userName(String email)
     // {
-    //     int a = email.indexOf("@");
-    //     String name = email.substring(0, a);
-    //     return name;
+    // int a = email.indexOf("@");
+    // String name = email.substring(0, a);
+    // return name;
 
     // }
-    static void change(int A[], int index, int value){
-        A[index]=value;
-        }
+    static void change(int X[], int index, int value) {
+        X[index] = value;
+    }
 
-     static void main(String[] args) {
+    static void change2(int x, int value) {
+        x = value;
+    }
+
+    public static void main(String[] args) {
         int A[] = { 1, 2, 3, 4, 5 };
 
-        change(A,2,20);
+        change(A, 2, 20);
 
-    for(int x:A)
-        System.out.println(x);
+        for (int x : A)
+            System.out.println(x);
     }
 
 }
